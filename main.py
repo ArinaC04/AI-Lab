cc26deb19262e2a107783b0462e63311e28cf0e5
Arina <arinacoroliuc@gmail.com>
2024-11-27 14:47:02 +0200

    new file:   MIDI/early_pieces_11_(nc)werths.mid 	new file:   MIDI/early_pieces_17_(nc)werths.mid 	new file:   MIDI/early_pieces_3_(nc)werths.mid 	new file:   MIDI/early_pieces_4_(nc)werths.mid new file:   MIDI/tchaikovsky_seasons_10_(c)oguri.mid 	modified:   __pycache__/functions.cpython-312.pyc 	modified:   main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import functions
 import user_interface 
 
 
-
+#degree of markov - parameter
 
 input_notes = ["C4", "C5"]
 instrument = 48
 roots = functions.create_trie()
+
+functions.print_trie(roots)
+
 melody = functions.generate(input_notes, 50, roots)
 functions.pitch_to_midi(melody, [400]*len(melody), instrument)
