af47724d9b5acb9632a881b69bd415415dbdf259
Arina <arinacoroliuc@gmail.com>
2024-12-03 14:27:15 +0200

    modified:   .coverage 	renamed:    implementation_document.md -> Project Documents/implementation_document.md 	renamed:    project-specification.md -> Project Documents/project-specification.md 	renamed:    testing_document.md -> Project Documents/testing_document.md 	renamed:    week1_report.md -> Weekly Reports/week1_report.md 	renamed:    week2_report.md -> Weekly Reports/week2_report.md 	renamed:    week3_report.md -> Weekly Reports/week3_report.md 	renamed:    week4_report.md -> Weekly Reports/week4_report.md 	renamed:    week5_report.md -> Weekly Reports/week5_report.md 	modified:   __pycache__/test_generate.cpython-312-pytest-8.3.3.pyc 	new file:   __pycache__/test_generate.cpython-312.pyc 	modified:   pyproject.toml 	modified:   test_generate.py

--- test_generate.py
+++ test_generate.py
@@ -3,7 +3,7 @@ from functions import generate, create_trie
 
 class test_generate(unittest.TestCase):
     def test_case_1(self):
-        degree = 10
+        degree = 4
         roots, comp = create_trie(degree)
         melody = generate(["C4", "C5"], 50, roots, degree)
         ok = True
