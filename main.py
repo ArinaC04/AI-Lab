3cde6663febaba5a08419fd6c9794df0dd74a0fd
Arina <arinacoroliuc@gmail.com>
2024-12-04 16:02:52 +0200

    modified:   __pycache__/user_interface.cpython-312.pyc 	modified:   main.py 	modified:   output.mid 	modified:   user_interface.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import functions
-import user_interface 
+from user_interface import display
 
 
 
 input_notes = ["C4", "C5"]
 instrument = 48
 degree = 4
-roots, comp = functions.create_trie(degree)
 
-#functions.print_trie(roots, degree)
+def func():
+    roots, comp = functions.create_trie(degree)
+    melody = functions.generate(input_notes, 50, roots, degree)
+    functions.pitch_to_midi(melody, [400]*len(melody), instrument)
 
-melody = functions.generate(input_notes, 50, roots, degree)
-functions.pitch_to_midi(melody, [400]*len(melody), instrument)
+display(func())
+
--- user_interface.py
+++ user_interface.py
@@ -1,12 +1,49 @@
 from tkinter import *
-root = Tk()
-w = Label(root, text='Music generation using Markov Chains')
-w.pack()
+
+def display(func):
+    root = Tk()
+    w = Label(root, text='Music generation using Markov Chains').grid(row=0)
+
+    Label(root, text='Input notes').grid(row=1)
+    T1 = Message(root, text = 'Write here the starting notes for the melody. They should range from C1 to C12')
+    T1.grid(row=2)
+    T1.config(bg='lightgreen')
 
 
-button = Button(root, text='Stop', width=25, command=root.destroy)
-button.pack()
 
 
 
-root.mainloop()
+    Label(root, text='Instrument').grid(row=3)
+    T2 = Text(root, height=2, width=30)
+    T2.grid(row=4)
+    T2.insert(END, 'What instrument would you like to use?\n ')
+    T2.config(state=DISABLED)
+
+
+
+    Label(root, text='Degree of Markov Chain').grid(row=5)
+    T3 = Text(root, height=2, width=30)
+    T3.grid(row=6)
+    T3.insert(END, 'How many notes would you like for the model to take into consideration when generating music?')
+    T3.config(state=DISABLED)
+
+
+    input = Entry(root)
+    instrument = Spinbox(root, from_=0, to=127)
+    degree = Entry(root)
+
+
+
+
+
+
+    input.grid(row=1, column=1)
+    instrument.grid(row=3, column=1)
+    degree.grid(row=5, column=1)
+
+
+    button = Button(root, text='Generate', width=25, command=func)
+
+
+
+    mainloop()
