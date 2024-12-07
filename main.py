762c5608bbd7b48826ad6cfdf0877df3e26d3355
Arina <arinacoroliuc@gmail.com>
2024-12-08 00:24:01 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ import re
 
 def func(input_notes, instrument, degree):
     input_notes = input_notes.get()
+    instrument = instrument.get()
+    degree = degree.get()
+    if input_notes == "" or instrument == "" or degree =="":
+        top = Toplevel()
+        top.title('Error')
+        t = Message(top, text = 'Wrong input. Try again')
+        t.grid(row=0)
+        t.config(bg='red')
+        top.mainloop()
+        return
     pitch_pattern = r'[A-G][#]?\d+'
     input = re.findall(pitch_pattern, input_notes)
-    instrument = int(instrument.get())
-    degree = int(degree.get())
+    instrument = int(instrument)
+    degree = int(degree)
     roots, comp = functions.create_trie(degree)
     melody = functions.generate(input, 50, roots, degree)
     functions.pitch_to_midi(melody, [400]*len(melody), instrument)
@@ -18,29 +28,26 @@ def func(input_notes, instrument, degree):
 root = Tk()
 w = Label(root, text='Music generation using Markov Chains').grid(row=0)
 
-Label(root, text='Input notes').grid(row=1)
-T1 = Message(root, text = 'Write here the starting notes for the melody. They should range from C1 to C12. Ex: C1, A3')
-T1.grid(row=2)
+T1 = Message(root, text = 'Write here the starting notes for the melody. They should range from C1 to C12. Ex: C5, C6')
+T1.grid(row=2, columnspan=2, sticky="WE")
 T1.config(bg='lightgreen')
 
+T2 = Message(root, text = 'What instrument would you like to use?\n Recommended: 48')
+T2.grid(row=4, columnspan=2, sticky="WE")
+T2.config(bg='lightgreen')
+
+T3 = Message(root, text = 'How many notes would you like for the model to take into consideration when generating music? Max 40')
+T3.grid(row=6, columnspan=2, sticky="WE")
+T3.config(bg='lightgreen')
+
+Label(root, text='Input notes').grid(row=1)
 Label(root, text='Instrument').grid(row=3)
-T2 = Text(root, height=2, width=30)
-T2.grid(row=4)
-T2.insert(END, 'What instrument would you like to use?\n ')
-T2.config(state=DISABLED)
-
-
-
 Label(root, text='Degree of Markov Chain').grid(row=5)
-T3 = Text(root, height=2, width=30)
-T3.grid(row=6)
-T3.insert(END, 'How many notes would you like for the model to take into consideration when generating music?')
-T3.config(state=DISABLED)
 
 
 input = Entry(root)
-instrument = Spinbox(root, from_=0, to=127)
-degree = Entry(root)
+instrument = Entry(root)
+degree = Spinbox(root, from_=0, to=40)
 
 
 input.grid(row=1, column=1)
@@ -49,7 +56,8 @@ degree.grid(row=5, column=1)
 
 
 button = Button(root, text='Generate', width=25, command=lambda: func(input, instrument, degree))
-button.grid(row = 7)
+button.grid(row = 7, columnspan=2, sticky="WE")
+
 
 
 
